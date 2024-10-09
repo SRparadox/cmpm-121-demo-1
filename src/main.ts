e40e47392e1145468103c457475c015ecb44d5c3
SRparadox <shazer.rizzo@gmail.com>
2024-10-08 23:06:11 -0700

    Step 3 complete

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,14 @@ let counter = 0;
 const obj = document.createElement("div");
 obj.innerText = "0 Lollipops";
 obj.className = "test";
-//document.getElementById("counterDisplay").appendChild(obj);
+//document.getElementById("counterDisplay").appendChild(obj); //WHY DOES THIS NOT ALLOW ME TO COMMITTTTTTTT!!!! AHH
+
+// Set up the timer to call the increment function every second (1000 milliseconds)
+function incrementCounter() {
+  counter += 1;
+  obj.innerText = counter + " Lollipops Lollipopped";
+}
+setInterval(incrementCounter, 1000);
 
 // Button HERE
 document.addEventListener("DOMContentLoaded", () => {
