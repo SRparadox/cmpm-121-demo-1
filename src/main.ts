3dec9821da7580b718b8977c747ad383737dd578
SRparadox <shazer.rizzo@gmail.com>
2024-10-08 22:55:28 -0700

    Step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,14 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 const gameName = "Clicker Game 101";
 document.title = gameName;
 
+// Counter Increase
+let counter = 0;
+
+const obj = document.createElement("div");
+obj.innerText = "0 Lollipops";
+obj.className = "test";
+//document.getElementById("counterDisplay").appendChild(obj);
+
 // Button HERE
 document.addEventListener("DOMContentLoaded", () => {
   const button = document.getElementById("myButton");
@@ -13,7 +21,9 @@ document.addEventListener("DOMContentLoaded", () => {
   if (button) {
     button.addEventListener("click", () => {
       console.log("Button was clicked!");
-      alert("Button clicked!");
+      counter += 1; //Increase the button
+      //alert(counter);
+      obj.innerText = counter + " Lollipops Lollipopped";
     });
   } else {
     console.error("Button not found!");
